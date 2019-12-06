2f9b0234927232eeba66b473ef17eb8c83b22cb5
Oleg Morozov <benjminmoore@gmail.com>
2019-12-06 15:53:09 +0300

    Add Scene Selector

---
+++ TestActors/Assets/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+    public SceneAsset scene;
+    public Button button;
+    
+    private void OnValidate()  => this.button = this.GetComponent<Button>();
+
+    private void Awake() => this.button.onClick.AddListener(() => SceneManager.LoadScene(this.scene.name));
+}
